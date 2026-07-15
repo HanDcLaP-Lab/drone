07698c16a0cde0cf5bd751faffa85ba5a5986ac1
KlemdRick_hy <3304924652@qq.com>
2026-07-15 19:53:38 +0800

    7.15b

--- project/code/image.h
+++ project/code/image.h
@@ -6,8 +6,8 @@
 #define IMG_CENTER_X (MT9V03X_W / 2.0f)
 #define IMG_CENTER_Y (MT9V03X_H / 2.0f)
 // 畸变中心 (Distortion Center)
-#define CAM_CX 111.4575192149
-#define CAM_CY  62.8987540474
+#define CAM_CX  111.5043628344
+#define CAM_CY  61.5005778257
 
 // 逆拉伸矩阵 (Inverse Stretch Matrix)
 #define INV_S11  1.0000000000
@@ -16,10 +16,10 @@
 #define INV_S22  1.0000000000
 
 // 映射多项式系数 (Mapping Coefficients)
-#define CAM_A0  49.3314081722
-#define CAM_A2  0.0069232179
-#define CAM_A3  -0.0008570029
-#define CAM_A4  0.0000113466
+#define CAM_A0  69.1036189537
+#define CAM_A2  -0.0050493933
+#define CAM_A3  -0.0002105636
+#define CAM_A4  0.0000005377
 // 宏定义
 #define STACK_SIZE 4096     // DFS 栈大小
 #define MAX_LIGHTS 20       // 最大识别灯光数量
